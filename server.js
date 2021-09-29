571eb993484fac52670c6c41e1c3522a02f5ec80
Kevin <kmgoldfarb@gmail.com>
2021-09-29 14:08:31 -0500

    Added HTTPS Server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const compression = require("compression");
 const usersRoutes = require("./routes/users-routes");
 const notesRoutes = require("./routes/notes-routes");
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 
 const app = express();
 app.use(express.json());
@@ -26,12 +28,25 @@ app.use((req, res, next) => {
 app.use("/api/users", usersRoutes);
 app.use("/api/notes", notesRoutes);
 
+const httpServer = httpServer.createServer(app);
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/everydaynotesapp.net/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/everydaynotesapp.net/fullchain.pem"
+    ),
+  },
+  app
+);
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_KEY}@cluster0.hqwcf.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    httpsServer.listen(443);
   })
   .catch((err) => {
     console.log(err);
